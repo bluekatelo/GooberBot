9adedf6235b5dc4ae60b4d816eaa94083e566222
Caleb Cohen <ccohen3@su.suffolk.edu>
2023-02-24 03:36:29 -0500

    Initial Commit

--- bot.js
+++ bot.js
@@ -1,14 +1,33 @@
-const Discord = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 const fs = require('fs');
 
-const client = new Discord.Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', () => {
-    console.log('Ready!');
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.login('MTA3MDg2MTkwODEyMDQ1NzI5OA.Gy8TZh.s-MHVoeGDiMuuP8r_L87nXKtzZEERIueZutk88');
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(`Error executing ${interaction.commandName}`);
+        console.error(error);
+    }
+});
+
+client.login(token);
 
 // Create a ReadableStream of s16le PCM audio
-const audio = connection.receiver.createStream(user, { mode: 'pcm' });
-audio.pipe(fs.createWriteStream('user_audio'));
+//const audio = connection.receiver.createStream(user, { mode: 'pcm' });
+//audio.pipe(fs.createWriteStream('user_audio'));
